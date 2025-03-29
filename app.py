591808d446e62d69baf98ecce8ad2a4e516211a4
harshavks96 <harshavks96@gmail.com>
2025-03-29 13:27:30 +0530

    code updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
@@ -8,15 +8,21 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    try:
+        return render_template('about.html')
+    except:
+        abort(404, description="About page template not found")
+
+@app.route('/services')
+def services():
+    try:
+        return render_template('services.html')
+    except:
+        abort(404, description="Services page template not found")
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
-@app.route('/services')
-def services():
-    return render_template('services.html')
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
